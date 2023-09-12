8c3b3c9d59aa64cb1efc00989123212c39cb76f4
Vanilson Magalhães Santos <vanilson.m.santos@gmail.com>
2023-09-12 17:00:25 -0300

    Updates authentication middleware when checking token

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -4,17 +4,15 @@ const CONSTANTS = require('../constants/api-errors');
 
 const verifyAuthentication = (req, res, next) => {
   const token = req.headers["x-access-token"];
-  console.log('token', token)
-  if (!token || token == null) {
-    return res.status(403).send({ message: CONSTANTS.TOKEN_REQUIRED });
+  if (token) {
+      try {
+      const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+      req.user = decoded;
+    } catch (err) {
+      return res.status(401).send({ message: CONSTANTS.INVALID_TOKEN });
+    }
+    return next();
   }
-  try {
-    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-    req.user = decoded;
-  } catch (err) {
-    return res.status(401).send({ message: CONSTANTS.INVALID_TOKEN });
-  }
-  return next();
 };
 
 module.exports = verifyAuthentication
